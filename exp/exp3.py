156e39d2336ad7919100086c283b7a549bfe3372
DK <34984338+LeoDK@users.noreply.github.com>
2018-02-24 14:21:08 +0100

    Update exp3.py

--- exp/exp3.py
+++ exp/exp3.py
@@ -8,41 +8,41 @@ Test du calcul de distance en Z.
 import sys
 sys.path.append("../")
 import cv2
-from autodrone import AutoDrone
+from autodrone import PCAutoDrone
 
 def calcZDist(ideal_roi, detected_roi):
-	if detected_roi:
-		d = ideal_roi[2] / detected_roi[3]
-		return d
-	return "NONE"
+    if detected_roi:
+        d = ideal_roi[2] / detected_roi[3]
+        return d
+    return "NONE"
 
 def followTarget(cam, tracker, ideal_roi):
-	cont, frame = cam.read()
-	while cont and cv2.waitKey(1) & 0xFF != ord('q'):
-		cont, frame = cam.read()
-		_, roi = tracker.update(frame)
-		cv2.rectangle(frame, (int(roi[0]), int(roi[1])), (int(roi[2]+roi[0]), int(roi[3]+roi[1])), (0,0,255), thickness=7)
-		cv2.imshow("Drone camera", frame)
-		print "d = {} m".format( calcZDist(ideal_roi, roi) )
+    cont, frame = cam.read()
+    while cont and cv2.waitKey(1) & 0xFF != ord('q'):
+        cont, frame = cam.read()
+        _, roi = tracker.update(frame)
+        cv2.rectangle(frame, (int(roi[0]), int(roi[1])), (int(roi[2]+roi[0]), int(roi[3]+roi[1])), (0,0,255), thickness=7)
+        cv2.imshow("Drone camera", frame)
+        print "d = {} m".format( calcZDist(ideal_roi, roi) )
 
 def showCam(cam):
-	cont, frame = cam.read()
-	while cont and cv2.waitKey(1) & 0xFF != ord(' '):
-		cont, frame = cam.read()
-		cv2.imshow("Drone camera", frame)
+    cont, frame = cam.read()
+    while cont and cv2.waitKey(1) & 0xFF != ord(' '):
+        cont, frame = cam.read()
+        cv2.imshow("Drone camera", frame)
 
 def chooseROI(cam):
-	roi = None
-	showCam(cam)
-	cont, frame = cam.read()
-	if cont:
-		roi = cv2.selectROI("Selection", frame, True, True)
-		cv2.destroyWindow("Selection")
-	return frame, roi
+    roi = None
+    showCam(cam)
+    cont, frame = cam.read()
+    if cont:
+        roi = cv2.selectROI("Selection", frame, True, True)
+        cv2.destroyWindow("Selection")
+    return frame, roi
 
 
 
-d = AutoDrone(True, verbosity=AutoDrone.QUIET)
+d = PCAutoDrone(True, verbosity=PCAutoDrone.QUIET)
 d.connect()
 d.enableCam()
 
@@ -52,9 +52,9 @@ tracker = cv2.TrackerMedianFlow_create()
 
 frame, roi = chooseROI(cam)
 if roi == None:
-	raise Exception("Mince...")
+    raise Exception("Mince...")
 if not tracker.init(frame, roi):
-	raise Exception("Mince2...")
+    raise Exception("Mince2...")
 
 followTarget(cam, tracker, roi)
 
