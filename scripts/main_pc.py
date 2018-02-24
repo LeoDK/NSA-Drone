ead8b24e329602289c64aaf339bcf3308877e058
DK <34984338+LeoDK@users.noreply.github.com>
2018-02-24 14:18:20 +0100

    Update main_pc.py

--- scripts/main_pc.py
+++ scripts/main_pc.py
@@ -3,36 +3,36 @@
 
 import sys
 sys.path.append("../")
-from autodrone import AutoDrone
+from autodrone import PCAutoDrone
 from time import sleep
 
-d = AutoDrone(False, min_speed=0.5, max_speed=2, verbosity=AutoDrone.QUIET)
+d = PCAutoDrone(False, min_speed=0.5, max_speed=2, verbosity=PCAutoDrone.QUIET)
 d.connect()
 
 try:
-	print "starting auto detection"
-	d.start()
-	sleep(5)
-	if len(sys.argv) == 2 and sys.argv[1] == '--dbg':
-		print "Debug mode"
-	else:
-		print "Taking off!!!"
-		d.takeOff()
-		sleep(4)
-		d.up()
-		sleep(0.5)
-		d.stop()
-	while d.isAlive():
-		sleep(0.5)
-	d.land()
+    print "starting auto detection"
+    d.start()
+    sleep(5)
+    if len(sys.argv) == 2 and sys.argv[1] == '--dbg':
+        print "Debug mode"
+    else:
+        print "Taking off!!!"
+        d.takeOff()
+        sleep(4)
+        d.up()
+        sleep(0.5)
+        d.stop()
+    while d.is_alive():
+        sleep(0.5)
+    d.land()
 
 except KeyboardInterrupt:
-	d.land()
-	d.inactivate()
-	print "DONE"
+    d.land()
+    d.inactivate()
+    print "DONE"
 
 except Exception as e:
-	d.land()
-	d.inactivate()
-	print "Error while launching autonomous drone"
-	print e
+    d.land()
+    d.inactivate()
+    print "Error while launching autonomous drone"
+    print e
