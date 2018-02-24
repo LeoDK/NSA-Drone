2b004b45e744340527c1a7914869ab294c9a4131
DK <34984338+LeoDK@users.noreply.github.com>
2018-02-24 14:18:45 +0100

    Update find_drone.py

--- scripts/find_drone.py
+++ scripts/find_drone.py
@@ -11,14 +11,14 @@ import sys
 filename = sys.argv[1]
 
 with open(filename, 'r') as f:
-	lines = tuple( l.strip() for l in f )
+    lines = tuple( l.strip() for l in f )
 
-	for i in range(1, len(lines)-3):
-		data = tuple( s.replace(',', '') for s in lines[i].split() )
-		#Tous les AR drones 2.0 possèdent un bssid qui commence par cette séquence
-		if data[0].startswith('90:03:B7'):
-			bssid = data[0]
-			channel = data[5]
-			essid = data[len(data)-1]
-			print "{}\n{}\n{}".format(bssid, channel, essid)
-			break
+    for i in range(1, len(lines)-3):
+        data = tuple( s.replace(',', '') for s in lines[i].split() )
+        #Tous les AR drones 2.0 possèdent un bssid qui commence par cette séquence
+        if data[0].startswith('90:03:B7'):
+            bssid = data[0]
+            channel = data[5]
+            essid = data[len(data)-1]
+            print "{}\n{}\n{}".format(bssid, channel, essid)
+            break
