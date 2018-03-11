183f3743afacda19b13bed5d990a75cd34f9d16a
DK <34984338+LeoDK@users.noreply.github.com>
2018-03-11 13:01:08 +0100

    Update pid.py

--- pid.py
+++ pid.py
@@ -7,29 +7,29 @@ class PID (object):
     """
 
     DEF_KP = 3
-    DEF_KI = 0.05
-    DEF_KD = 1
+    DEF_KI = 0.8
+    DEF_KD = 0.1
 
     def __init__(self, kp=DEF_KP, ki=DEF_KI, kd=DEF_KD):
         self.kp = kp
         self.ki = ki
         self.kd = kd
-        self.integral.last_error = 0
-        self.derivative.last_error = 0
-        self.error_area = 0
+        self.last_integral_error = 0
+        self.integral_error = 0
+        self.last_derivative_error = 0
 
     def proportionnal(self, error):
         return error
 
     def integral(self, error, delta_t):
         # Méthode d'approximation des trapèzes
-        self.area_error += (error - self.integral.last_error) * delta_t + self.integral.last_error * delta_t
-        self.integral.last_error = error
-        return self.area_error
+        self.integral_error += (error - self.last_integral_error) * delta_t + self.last_integral_error * delta_t
+        self.last_integral_error = error
+        return self.integral_error
 
     def derivative(self, error, delta_t):
-        ret = (error - self.derivative.last_error) / delta_t
-        self.derivative.last_error = error
+        ret = (error - self.last_derivative_error) / delta_t
+        self.last_derivative_error = error
         return ret
 
     def getOrder(self, error, delta_t):
